873e2beb255e15d2f4d61fc8a0c45077ac004895
DevCharles <charlesipn@gmail.com>
2022-06-10 04:53:50 -0500

    setupexample

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { RedocModule, RedocOptions } from 'nestjs-redoc';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  const redocOptions: RedocOptions = {
+    title: 'Hello Nest',
+    logo: {
+      url: 'https://redocly.github.io/redoc/petstore-logo.png',
+      backgroundColor: '#F0F0F0',
+      altText: 'PetStore logo',
+    },
+    sortPropsAlphabetically: true,
+    hideDownloadButton: false,
+    hideHostname: false,
+    auth: {
+      enabled: true,
+      user: 'admin',
+      password: '123',
+    },
+    tagGroups: [
+      {
+        name: 'Core resources',
+        tags: ['cats'],
+      },
+    ],
+  };
+  // Instead of using SwaggerModule.setup() you call this module
+  await RedocModule.setup('/docs', app, document, redocOptions);
+
+  // SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
