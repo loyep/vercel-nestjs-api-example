995ed180f0d0aba6fff5f1176a0c1443986c2a4e
DevCharles <charlesipn@gmail.com>
2022-06-10 05:03:59 -0500

    more endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,23 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): object {
+    return {
+      message: 'NestJS API Vercel example',
+    };
+  }
+
+  @Get('/other')
+  other(): object {
+    return {
+      message: 'other',
+    };
+  }
+
+  @Post()
+  post(): object {
+    return {
+      message: 'post example',
+    };
   }
 }
