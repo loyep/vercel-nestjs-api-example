e7320f58c00aebeb69a01a740e0be14ab3fe7423
Zack <lorenx@163.com>
2022-07-31 21:34:58 +0800

    feat: 更新

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module, Post } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostService } from './post.service';
-import { PrismaService } from './prisma.service';
 import { UserService } from './user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PostService, UserService, PrismaService],
+  providers: [AppService, PostService, UserService],
 })
 export class AppModule {}
